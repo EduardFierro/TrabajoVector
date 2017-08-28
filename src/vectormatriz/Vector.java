d971831956341a022032ede87bc93781cefac80b
Pavilion 15 <Pavilion 15@Eduard>
2017-08-28 16:29:33 -0500

    commit inicial

--- src/vectormatriz/Vector.java
+++ src/vectormatriz/Vector.java
@@ -12,72 +12,37 @@ import java.util.Scanner;
  * @author Pavilion 15
  */
 public class Vector {
-    int []vec;
-    private int tam;
+    private int b, aux;
+    int []vect;
+    public void vector(){
+        
+        Scanner opc2 = new Scanner(System.in);
+        System.out.print(" Digite el tamaño del vector: ");
+        Scanner opc1 = new Scanner(System.in);
+        b= opc1.nextInt();
+        vect = new int[b];
+        for(int i=0;i<b;i++){
+            System.out.print(" Digite numero: ");
+            vect[i] = opc2.nextInt();
 
-    public Vector( int tam) {
-        this.setTam(tam);
-    }
-
-    public int[] getVec() {
-        return vec;
-    }
-
-    public void setVec(int[] vec) {
-        this.vec = vec;
-    }
-
-    public int getTam() {
-        return tam;
-    }
-
-    public void setTam(int tam) {
-        this.tam = tam;
-    }
-    public void Menu(){
-        int a,b,c,d, mayor=0,menor=0;
-        int []vect={};
-    
-        System.out.println("           Menu");
-        System.out.println(" Va crear vector o matriz?");
-        System.out.println("1.Vector ");
-        System.out.println("2.Matriz ");
-        System.out.print(": ");
-        Scanner opc = new Scanner(System.in);
-        a= opc.nextInt();
-        switch(a){
-            case 1:
-                System.out.print(" Digite el tamaño del vector: ");
-                Scanner opc1 = new Scanner(System.in);
-                b= opc1.nextInt();
-                
-                for(int i=0;i<b;i++){
-                    System.out.print(" Digite numero: ");
-                     Scanner opc2 = new Scanner(System.in);
-                    vect[i] = opc2.nextInt();
-                     
-
-                }
-                System.out.println(" En que orden desea el vector: ");
-                System.out.println(" 1.Ascendente");
-                System.out.println(" 2.Descendente");
-                System.out.println(" Digite Opcion: ");
-                Scanner opc3 = new Scanner(System.in);
-                c= opc3.nextInt();
-                switch(c){
-                    case 1:
-                        for(int i=0;i<b;i++){
-                            if(vect[i]<vect[i+1]){
-                                
-                            }
-                        }
-                        break;
-                }
-                
-                
-            break;
-                 
         }
-
     }
+    public int ascendente(){
+        int i,j;
+        for(i=0;i<b;i++){
+            for(j=0;j<b;j++){
+                if(vect[j]>vect[j+1]){
+                    aux=vect[j];
+                    vect[j]=vect[j+1];
+                    vect[j+1]= aux;
+                }
+            }
+        }    
+        for(i=0;i<b;i++){
+            System.out.println(vect[i]);
+        }
+    
+        return 0;
+    }
+
 }
--- src/vectormatriz/VectorMatriz.java
+++ src/vectormatriz/VectorMatriz.java
@@ -17,8 +17,32 @@ public class VectorMatriz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-         Vector ve= new Vector(12);
+        int a,c;
+         
+        System.out.println("           Menu");
+        System.out.println(" Va crear vector o matriz?");
+        System.out.println("1.Vector ");
+        System.out.println("2.Matriz ");
+        System.out.print(": ");
+        Scanner opc = new Scanner(System.in);
+        a= opc.nextInt();
+        switch(a){
+            case 1:
+                Vector p =new Vector();
+                p.vector();
+                System.out.println(" En que orden desea el vector ");
+                System.out.println(" 1.Ascendente");
+                System.out.println(" 2.Descendente");
+                System.out.print(" Digite Opcion: ");
+                Scanner opc3 = new Scanner(System.in);
+                c= opc3.nextInt();
+                switch(c){
+                    case 1: 
+                       p.ascendente();
+                    break;
+                }
+            break;
+        }
     }
     
 }
